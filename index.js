131116eab8929a25404a5101ac9582a69672f63b
anventech <dev.anventec@gmail.com>
2020-11-09 20:04:57 -0500

    :thumbsup: programming-quotes-api agregada globalmente.

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * UniWrapper developed by: Anventec#3735 (Discord) / UniWrapper desarrollado por: Anventec#3735
+ * Project GitHub / GitHub del proyecto: https://github.com/anventech/uniwrapper
+ * Personal GitHub / GitHub personal: https://github.com/anventech
+ */
+
+module.exports = {
+    quotes: {
+        programmingQuotesAPI: require('./wrappers/quotes/programmingQuotesAPI.js')
+    }
+};
